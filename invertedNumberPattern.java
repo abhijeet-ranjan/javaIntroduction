604904ddd66f0f29a1ccff99b83b89e0fda1383e
abhijeet-ranjan <72022232+abhijeet-ranjan@users.noreply.github.com>
2020-09-28 19:23:59 +0530

    Create invertedNumberPattern.java
    666666
    55555 
    4444
    333
    22
    1

---
+++ invertedNumberPattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		int i = 1;
+		while(i <= n){
+			int j = 1;
+				while(j <= (n-i+1)){
+					System.out.print(n-i+1);
+					j++;
+				}
+				System.out.println();
+				i++;
+        }				
+	}
+}
